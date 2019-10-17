8f2c45a3b95c59f21815cf6e3dae8af3557cbe2f
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-10-17 15:43:29 +0700

    Created bootcamp points

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dependencies
+
+express, dotenv, nodemon
--- server.js
+++ server.js
@@ -7,6 +7,37 @@ dotenv.config({ path: "./config/config.env" });
 // Init the app var with express
 const app = express();
 
+// Get bootcamps - show all bootcamps
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps." });
+});
+
+// Get bootcamp - show one bootcamp
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+// Post bootcamp - add a new bootcamp
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp." });
+});
+
+// Put bootcamp - update a bootcamp
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+// Delete a bootcamp
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
