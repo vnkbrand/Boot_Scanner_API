a626a7a4bb0fb9e705382ad8193c678068096a3c
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-11-07 13:51:52 +0700

    Globalised async await

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -96,6 +97,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function(next) {
+  console.log("Slugify ran", this.name);
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
