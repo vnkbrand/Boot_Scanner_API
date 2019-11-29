b021d89d7a4726b9df1a0138db23939521270465
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-11-29 14:20:13 +0700

    Added advanceResults middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,26 +8,19 @@ const Bootcamp = require("../models/Bootcamp");
 //  @route    GET /api/v1/bootcamps/:bootcampId/courses
 // @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
     // Finds bootcamp with courses
-    query = Course.find({ bootcamp: req.params.bootcampId });
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
+    });
   } else {
     // Does not find bootcamp - get ALL courses
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description'
-    });
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses
-  });
 });
 
 //  @desc     Get single course
--- routes/courses.js
+++ routes/courses.js
@@ -3,12 +3,20 @@ const {
   getCourses, getCourse, addCourse, updateCourse, deleteCourse
 } = require("../controllers/courses");
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
-  .get(getCourses)
+  .get(advancedResults(Course, {
+    path: 'bootcamp',
+    select: 'name description'
+  }),
+    getCourses)
   .post(addCourse);
+
 router
   .route('/:id')
   .get(getCourse)
