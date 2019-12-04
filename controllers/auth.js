32779ff1df2fe189c64342a4907c5f7e4f6adc76
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-12-04 14:34:57 +0700

    Auth Protect Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,10 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 const jwt = require('jsonwebtoken');
-//  @desc     Register user
-//  @route    POST /api/v1/auth/register
-// @access    Public
 
+// @desc      Register user
+// @route     POST /api/v1/auth/register
+// @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
@@ -17,20 +17,17 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token: token });
+  sendTokenResponse(user, 200, res);
 });
 
-//  @desc     Login user
-//  @route    POST /api/v1/auth/login
-// @access    Public
 
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
-  // Validate email and password
+  // Validate emil & password
   if (!email || !password) {
     return next(new ErrorResponse('Please provide an email and password', 400));
   }
@@ -49,8 +46,55 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token: token });
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
+
+// @desc      Get current logged-in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: req.user
+  });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -8,6 +8,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router
   .route('/')
   .get(advancedResults(Course, {
@@ -15,13 +17,13 @@ router
     select: 'name description'
   }),
     getCourses)
-  .post(addCourse);
+  .post(protect, addCourse);
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 
 module.exports = router;
