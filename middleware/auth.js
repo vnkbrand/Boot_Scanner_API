152600a65b52b67a2093f50a3ac9ed5d1e52f1bf
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-12-08 16:42:54 +0700

    Added Mongo Sanitize

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
 
-  // else if(req.cookies.token) {
-  //   token = req.cookies.otken
+  // Set token from cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // Make sure the token exists
