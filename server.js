561f1fa5fa3debadf59d9badcb8254c3c1b01a1f
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-12-09 13:55:54 +0700

    Ready for Deployment

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('xss-clean');
+const xss = require('helmet');
+// const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -41,6 +46,25 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers (helmet)
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+// const limiter = rateLimit({
+//   windowMs: 10 * 60 * 1000,
+//   max: 100
+// });
+// app.use(limiter());
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
